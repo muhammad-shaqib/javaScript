c0f1664d1721d45fccce1efc19282e6ee9a36968
muhammad-shaqib <engr.muhammadshahqib@gmail.com>
2024-05-09 22:36:43 +0500

    upload_que01

--- javaScript_practice_que/que-01/main.js
+++ javaScript_practice_que/que-01/main.js
@@ -1,1 +1,9 @@
-
+// practice que No # 01
+// Create a const object called "Product" to store information i.e name, rating, offer, price
+const Product = {
+    tittle : "Ball Pen",
+    rating : 4,
+    offer : 5,
+    price : 270,
+};
+console.log(Product);
