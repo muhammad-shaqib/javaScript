9b8c270217dfc3f00a83f5ce162a7a6a6a28306a
muhammad-shaqib <engr.muhammadshahqib@gmail.com>
2024-05-10 22:56:01 +0500

    que-02 upload

---
+++ javaScript_practice_que/que-02/main.js
@@ -0,0 +1,10 @@
+// practice que No # 02
+// Create a const object called "Profile" to store information i.e username, isFollow, follower, following
+
+const Profile = {
+    userName : "Afnan Khan",
+    isFollow : false,
+    followers : 500,
+    following : 20,
+};
+console.log(Profile);
