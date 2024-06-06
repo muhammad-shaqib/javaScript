1c5a5cd13e502024da7924ffe7c620469099ce35
muhammad-shaqib <engr.muhammadshahqib@gmail.com>
2024-06-06 13:04:26 +0500

    que-05 Uploade

--- javaScript_practice_que/que-05/script.js
+++ javaScript_practice_que/que-05/script.js
@@ -1,1 +1,82 @@
-
+// operators in js
+// Arithmetic operators
+// plus
+{
+    let a = 5;
+    let b = 2;
+    console.log("a + b = ", a + b); // 5 + 2 = 7
+    };
+    
+    // minus
+    {
+        let a = 5;
+        let b = 2;
+        console.log("a - b = ", a - b); // 5 - 2 = 3
+    };
+    
+    // Multiply
+    {
+        let a = 5;
+        let b = 2;
+        console.log("a * b = ", a * b); // 5 * 2 = 10
+    };
+    
+    // Divide
+    {
+        let a = 5;
+        let b = 2;
+        console.log("a / b = ", a / b); // 5 / 2 = 2.5
+    };
+    // There are different way to print operators in js i think is good way to print operators from learning.
+    // 2nd way to print operators
+    
+    console.log("5 + 2 =", 5 + 2); //7
+    console.log("5 - 2 =", 5 - 2); //3
+    console.log("5 * 2 =", 5 * 2); //10
+    console.log("5 / 2 =", 5 / 2); //2.5
+    
+    // Modulus (Remends)
+    // symbol (%)
+    {
+        let a = 5;
+        let b = 2;
+        console.log("a % b = ", a % b); // 5 % 2 = 1
+    };
+    
+    // Exponentiation( it means power 5^5=25)
+    // symbol (**)
+    {
+        let a = 5;
+        let b = 2;
+        console.log("a ** b = ", a ** b); // 5 ** 2 = 25
+    };
+    
+    /* Unary Operators
+    two types i.e
+    1. Increment (a++) means a = a+1
+    Post Increment (a++) means a = a+1
+    Pre Increment (++a) means a = a+1
+    2. Decrement (a--) means a = a-1
+    Post Decrement (a--) means a = a-1
+    Pre Decrement (--a) means a = a-1
+    */
+    // Increment
+    {
+        let a = 5;
+        // value well be add and then print in next line in this condition
+        console.log("a++ = ", a++); // 5 (Post Increment)
+        console.log("a = ", a); // 6
+        console.log("++a = ", ++a); // 7  (Pre Increment)
+        // In this condition value directly add and print in same line
+    };
+    
+    // Decrement
+    {
+        let a = 5;
+        // value well be minus and then print in next line in this condition
+        console.log("a-- = ", a--); // 5 (Post Decrement)
+        console.log("a = ", a); // 4
+        console.log("--a = ", --a); // 3 (Pre Decrement)
+        // In this condition value directly add and print in same line
+    
+    };
