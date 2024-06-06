ad5f1e1b6d94241d658e1dc7fef665d981499e65
muhammad-shaqib <engr.muhammadshahqib@gmail.com>
2024-06-06 12:38:21 +0500

    update  the code

--- javaScript_practice_que/que-03/main.js
+++ javaScript_practice_que/que-03/main.js
@@ -1,26 +1,39 @@
-// Example-01
-/* OR Operator (||)
-The || operator returns true if at least one condition is true.*/
-let x = 5;
-let y = 3;
-if (x > 2 || y > 2) {
-    console.log("True");
-}
+/* Logical Operators
+Three Types
 
-/*NOT Operator (!)
+1. OR Operator 
+Symbol (||)
+The || operator returns true if at least one condition is true.*/
+// Example-01
+{
+let a = 6;
+let b = 5;
+let cond1 = (a > 2); //true
+let cond2 = (b > 6); //false
+console.log("cond1 || cond2 =", cond1 || cond2); //true
+// another way to print.
+console.log("cond1 || cond2 =", a > b || b > 6); //true
+};
+
+/*2. NOT Operator (!)
 The ! operator returns true if the condition is false, and false if the condition is true.*/
 // Example-02
-let b = 5;
-if (!(b > 10)) {
-    console.log("b is not greater than 10");
-}
+{
+    let a = 6;
+    let b = 5;
+    console.log("! 6 > 5 =", !(6 > 5)); //false
+    // not operator need only one condition to print value.
+};
 
-/*Combining Logic Operators
-You can combine multiple logic operators to create more complex conditions.*/
+//3. Logic && (And) Operators
+// Both condition should be true, if one condition is false the ans will be false.
 // Example-03
-let e = 5;
-let f = 3;
-let z = 2;
-if (e > 2 && (f > 2 || z > 2)) {
-    console.log("The condition is true");
-}
+{
+    let a = 6;
+    let b = 5;
+    let cond1 = (a > b); //true
+    let cond2 = (a === 6); //true
+    console.log("cond1 && cond2 =", cond1 && cond2); //true
+    // another way to print
+    console.log("cond1 && cond2 =", a > b && a === 6); //true
+};
